0ca1b8e7fda3af0d6d4dbae7ed534eea0ac1bc5e
Leenobalaleeno <g00087238@aus.edu>
2023-09-07 11:54:56 +0400

    feature B added!

--- src/Lab1/Exercise3.java
+++ src/Lab1/Exercise3.java
@@ -4,7 +4,10 @@ public class Exercise3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello LEEN's World");
 	}
-
+	public void memberBfunccc()
+	{
+		System.out.println("\nbyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+	}
 }
